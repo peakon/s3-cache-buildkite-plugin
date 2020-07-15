f711c23291bb7050875822ada768c7cd28a40fed
Vitalii Grygoruk <vitalii@peakon.com>
2020-07-15 21:14:54 +0200

    added shellcheck and make it green

--- lib/functions.bash
+++ lib/functions.bash
@@ -2,7 +2,8 @@
 
 # returns a JSON object with plugin configuration 
 function getPluginConfig {
-  local config=$(echo $BUILDKITE_PLUGINS | jq '. | map(to_entries) | flatten | map(select(.key | match("peakon/s3-cache";"i"))) | map(.value)')
+  local config
+  config=$(echo "$BUILDKITE_PLUGINS" | jq '. | map(to_entries) | flatten | map(select(.key | match("peakon/s3-cache";"i"))) | map(.value)')
   if [[ "$config" == "null" ]]; then
       echo "peakon/s3-cache plugin is misconfigured"
       exit 1
@@ -13,35 +14,38 @@ function getPluginConfig {
 
 # returns a JSON with restore config
 function getRestoreConfig {
-  local pluginConfig=$(getPluginConfig)
-  echo $pluginConfig | jq --arg key0 "${BUILDKITE_PLUGIN_S3_CACHE_RESTORE_0_KEYS_0:-}" 'map(select((.restore[0].keys[0] == $key0) and (.restore[0].keys[0] | length > 0))) | map(.restore) | flatten'
+  local pluginConfig
+  pluginConfig=$(getPluginConfig)
+  echo "$pluginConfig" | jq --arg key0 "${BUILDKITE_PLUGIN_S3_CACHE_RESTORE_0_KEYS_0:-}" 'map(select((.restore[0].keys[0] == $key0) and (.restore[0].keys[0] | length > 0))) | map(.restore) | flatten'
 }
 
 # returns a JSON with save config
 function getSaveConfig {
-  local pluginConfig=$(getPluginConfig)
-  echo $pluginConfig | jq --arg key0 "${BUILDKITE_PLUGIN_S3_CACHE_SAVE_0_KEY:-}" 'map(select((.save[0].key == $key0) and (.save[0].key | length > 0))) | map(.save) | flatten'
+  local pluginConfig
+  pluginConfig=$(getPluginConfig)
+  echo "$pluginConfig" | jq --arg key0 "${BUILDKITE_PLUGIN_S3_CACHE_SAVE_0_KEY:-}" 'map(select((.save[0].key == $key0) and (.save[0].key | length > 0))) | map(.save) | flatten'
 }
 
 # $1 template string
 function getCacheKey {
-  local cache_key=$1
+  local cache_key
+  cache_key="$1"
   while [[ "$cache_key" == *"{{"* ]]; do
-    cache_key_prefix=$(echo "$cache_key" | sed -e 's/{.*//')
     template_value=$(echo "$cache_key" | sed -e 's/^[^\{{]*[^A-Za-z\.]*//' -e 's/\s*}}.*$//' | tr -d \' | tr -d \")
-
-    local result=unsupported
+    local result
+    result=unsupported
     if [[ $template_value == *"checksum"* ]]; then
-      checksum_argument=$(echo "$template_value" | sed -e 's/checksum*//')
       function=${template_value/"checksum"/"sha1sum"}
       result=$($function | awk '{print $1}')
     elif [[ $template_value == *".Environment.BUILDKITE_"* ]]; then
-      local var_name=$(echo $template_value | sed -e 's/\.Environment\.//')
+      local var_name
+      var_name="${template_value//\.Environment\./}"
       result=$(echo "${!var_name}" | sed -e 's/[\/\\\]/_/g')
-    elif [[ $template_value == "epoch" ]]; then
+    elif [[ "$template_value" == "epoch" ]]; then
       result=$(date +%s)
     fi
-     cache_key=$(echo $cache_key | sed -e "s/[\{][\{]\([^}}]*\)[\}][\}]/$result/")
+    # shellcheck disable=SC2001
+    cache_key=$(echo "$cache_key" | sed -e "s/[\{][\{]\([^}}]*\)[\}][\}]/$result/")
   done
 
   echo "$cache_key"
@@ -49,12 +53,12 @@ function getCacheKey {
 
 # $1 saveConfig
 function getCacheItemsForSave {
-  echo $1 | jq -r '.[] | "\(.key | @base64) \(.paths | join(" ") | @base64) \(.overwrite // "false") \(.when // "on_success")"'
+  echo "$1" | jq -r '.[] | "\(.key | @base64) \(.paths | join(" ") | @base64) \(.overwrite // "false") \(.when // "on_success")"'
 }
 
 # $1 restoreConfig
 function getCacheItemsForRestore {
-  echo $1 | jq -r '.[] | "\(.keys | [.[] | @base64] | join(" "))"'
+  echo "$1" | jq -r '.[] | "\(.keys | [.[] | @base64] | join(" "))"'
 }
 
 function s3ObjectKey {
@@ -62,26 +66,30 @@ function s3ObjectKey {
 }
 
 function s3Path {
-  local s3Key=$(s3ObjectKey "$1")
+  local s3Key
+  s3Key=$(s3ObjectKey "$1")
   echo "s3://${BUILDKITE_PLUGIN_S3_CACHE_BUCKET_NAME}/${s3Key}"
 }
 
 # $1 - cacheKey
 function s3Exists {
-  local s3Key=$(s3ObjectKey "$1")
+  local s3Key
+  s3Key=$(s3ObjectKey "$1")
   local s3KeyExists="true"
-  aws s3api head-object --bucket "$BUILDKITE_PLUGIN_S3_CACHE_BUCKET_NAME" --key $s3Key &>/dev/null || s3KeyExists=false
+  aws s3api head-object --bucket "$BUILDKITE_PLUGIN_S3_CACHE_BUCKET_NAME" --key "$s3Key" &>/dev/null || s3KeyExists=false
   echo "$s3KeyExists"
 }
 
 # $1 - cacheKey
 # $2 - a string with space separated file paths
 function s3Upload {
-  local s3_path=$(s3Path "$1")
-  local localPaths=($2)
+  local s3_path
+  local localPaths
+  s3_path=$(s3Path "$1")
+  localPaths=("$2")
   set +e
-  tar --ignore-failed-read -cz ${localPaths[@]} | aws s3 cp - "$s3_path"
-  if [ $? -ne 0 ]; then
+  # shellcheck disable=SC2068
+  if ! (tar --ignore-failed-read -cz ${localPaths[@]} | aws s3 cp - "$s3_path"); then
     echo "false"
   else
     echo "true"
@@ -91,10 +99,10 @@ function s3Upload {
 
 # $1 - cacheKey
 function s3Restore {
-  local s3_path=$(s3Path "$1")
+  local s3_path
+  s3_path=$(s3Path "$1")
   set +e
-  output=$(aws s3 cp "$s3_path" - | tar -xz)
-  if [ $? -ne 0 ]; then
+  if ! aws s3 cp "$s3_path" - | tar -xz > /dev/null; then
     echo "false"
   else
     echo "true"
@@ -105,28 +113,30 @@ function s3Restore {
 function makeTempFile {
   tempFile=$(mktemp)
   cleanup() {
-    rm $tempFile
+    rm "$tempFile"
   }
   trap cleanup EXIT
   echo "$tempFile"
 }
 
 function saveCache {
-  local saveConfig=$(getSaveConfig)
+  local saveConfig
+  saveConfig=$(getSaveConfig)
   if [[ "$saveConfig" == "[]" ]]; then
       echo "No save config found, skipping"
       return
   fi
 
-  local tempFile=$(makeTempFile)
-  getCacheItemsForSave "$saveConfig" > $tempFile
+  local tempFile
+  tempFile=$(makeTempFile)
+  getCacheItemsForSave "$saveConfig" > "$tempFile"
 
-  while read keyTemplateBase64 pathsBase64 overwrite when
+  while read -r keyTemplateBase64 pathsBase64 overwrite when
   do
-    keyTemplate=$(echo $keyTemplateBase64 | base64 -d)
+    keyTemplate=$(echo "$keyTemplateBase64" | base64 -d)
     
     key=$(getCacheKey "$keyTemplate")
-    paths=$(echo $pathsBase64 | base64 -d)
+    paths=$(echo "$pathsBase64" | base64 -d)
     
     if [[ ! "$when" =~ ^(on_success|on_failure|always)$ ]]; then
       echo ":warn: invalid value specified for 'when' option ($when), ignoring"
@@ -144,7 +154,8 @@ function saveCache {
     fi
     
     if [[ "$when" =~ $uploadConditions ]]; then
-      local alreadyOnS3=$(s3Exists "$key")
+      local alreadyOnS3
+      alreadyOnS3=$(s3Exists "$key")
       if [[ "$alreadyOnS3" == "false" ]]; then
         echo "Uploading new cache for key: $key"
       elif [[ "$overwrite" == "true" ]]; then
@@ -153,42 +164,49 @@ function saveCache {
         echo "Cache already exists (and will not be updated) for key: $key"
         continue
       fi
-      local isSaved=$(s3Upload "$key" "$paths")
+      local isSaved
+      isSaved=$(s3Upload "$key" "$paths")
       if [[ "$isSaved" == "true" ]]; then
         echo "Uploaded new cache for key: $key"
       else
-        echo "Failed to upload new cache for key: $ke"
+        echo "Failed to upload new cache for key: $key"
       fi
     else
       echo "Skipping cache upload for key: $key ('when' condition is not met)"
     fi
-  done < $tempFile
+  done < "$tempFile"
 }
 
 function restoreCache {
-  local restoreConfig=$(getRestoreConfig)
+  local restoreConfig
+  restoreConfig=$(getRestoreConfig)
   if [[ "$restoreConfig" == "[]" ]]; then
       echo "No restore config found, skipping"
       return
   fi
   
-  local tempFile=$(makeTempFile)
-  getCacheItemsForRestore "$restoreConfig" > $tempFile
+  local tempFile
+  tempFile=$(makeTempFile)
+  getCacheItemsForRestore "$restoreConfig" > "$tempFile"
 
-  while read cacheItemKeyTemplates
+  while read -r cacheItemKeyTemplates
   do
+    # shellcheck disable=SC2206
     local cacheItemKeysTemplatesArray=($cacheItemKeyTemplates)
     for cacheItemKeyTemplate in "${cacheItemKeysTemplatesArray[@]}"
     do
-      local cacheItemKeyTemplateDecoded=$(echo "$cacheItemKeyTemplate" | base64 -d)
-      local cacheKey=$(getCacheKey "$cacheItemKeyTemplateDecoded")
-      local isRestored=$(s3Restore "$cacheKey")
+      local cacheItemKeyTemplateDecoded
+      local isRestored
+      local cacheKey
+      cacheItemKeyTemplateDecoded=$(echo "$cacheItemKeyTemplate" | base64 -d)
+      cacheKey=$(getCacheKey "$cacheItemKeyTemplateDecoded")
+      isRestored=$(s3Restore "$cacheKey")
       if [[ "$isRestored" == "true" ]]; then
         echo "Successfully restored $cacheKey"
         break
       else
-        echo "Failed to restore $cacheKey"
+        echo "Failed to restore $cacheKey. The following error occured: ${isRestored}"
       fi
     done
-  done < $tempFile
+  done < "$tempFile"
 }
--- tests/pre-command.bats
+++ tests/pre-command.bats
@@ -42,9 +42,9 @@ function teardown() {
 @test "Pre-command succeeds to restore from a fallback key if first key is missing" {
   export BUILDKITE_PLUGINS="[{\"github.com/peakon/s3-cache-buildkite-plugin#v1.5.0\":{\"restore\":[{\"keys\":[\"cache-key-missing\",\"cache-key-exists\"]}]}}]"
   export BUILDKITE_PLUGIN_S3_CACHE_RESTORE_0_KEYS_0=cache-key-missing
-  
+
   stub aws \
-    "s3 cp s3://${BUILDKITE_PLUGIN_S3_CACHE_BUCKET_NAME}/${BUILDKITE_ORGANIZATION_SLUG}/${BUILDKITE_PIPELINE_SLUG}/cache-key-missing.tar.gz - : echo 'failed' && exit 1" \
+    "s3 cp s3://${BUILDKITE_PLUGIN_S3_CACHE_BUCKET_NAME}/${BUILDKITE_ORGANIZATION_SLUG}/${BUILDKITE_PIPELINE_SLUG}/cache-key-missing.tar.gz - : echo failed; exit 1" \
     "s3 cp s3://${BUILDKITE_PLUGIN_S3_CACHE_BUCKET_NAME}/${BUILDKITE_ORGANIZATION_SLUG}/${BUILDKITE_PIPELINE_SLUG}/cache-key-exists.tar.gz - : echo true"
   stub tar \
     "-xz : echo true" \
